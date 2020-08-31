3b1c605fc7dd795788ae6b98be92ece01f489c4c
fzbdeqq <438267278@qq.com>
2020-08-31 20:28:55 +0800

    模板方法

--- src/com/snail/headFirst/chapter8/CoffeeWithHook.java
+++ src/com/snail/headFirst/chapter8/CoffeeWithHook.java
@@ -1,5 +1,9 @@
 package com.snail.headFirst.chapter8;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @Author: SnailBBB
  * @Description:
@@ -18,8 +22,30 @@ public class CoffeeWithHook extends CaffeineBeverage {
 
     @Override
     boolean customerWantsCondiments() {
-        return false;
+        String answer=getUserInput();
+        if(answer.toLowerCase().startsWith("y")){
+            return true;
+        }else {
+            return false;
+        }
+
     }
+
+    private String getUserInput() {
+        String answer=null;
+        System.out.println("would you like milk and sugar with your coffee(y/n)?");
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        try {
+            answer=in.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(answer==null){
+            answer="no";
+        }
+        return answer;
+    }
+
     //    void prepareRecipe(){
 //        boilWater();
 //        brewCoffeeGrinds();
